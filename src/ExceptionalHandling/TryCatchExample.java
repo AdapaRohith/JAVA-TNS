2766b58bab209040a03217a08f042e05c9450614
adapa <adapa@DESKTOP-H9377KG>
2026-09-15 10:20:07 +0530

    ExceptionHandling

---
+++ src/ExceptionalHandling/TryCatchExample.java
@@ -0,0 +1,19 @@
+package ExceptionalHandling;
+
+public class TryCatchExample {
+	public static void main(String[]args) {
+		try {
+			int a = 10;
+			int b = 0;
+			System.out.println(a/b);
+		}
+		catch(ArithmeticException e) {
+			
+			
+			System.out.println(e);
+			System.out.println("Cannot divide with 0");
+			
+		}
+	}
+
+}
